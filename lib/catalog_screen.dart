18a649563664d7fe85ad43a0056ff489b00a41d7
DariaZvezdochkina <57398072+DariaZvezdochkina@users.noreply.github.com>
2022-03-09 22:50:20 +0200

    Update catalog_screen.dart

--- lib/catalog_screen.dart
+++ lib/catalog_screen.dart
@@ -8,22 +8,27 @@ class CatalogScreen extends StatelessWidget {
     return Scaffold(
         body: GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, childAspectRatio: 1.5),
-            itemCount: 3,
+                crossAxisCount: 2, childAspectRatio: 1),
+            itemCount: 2,
             itemBuilder: (BuildContext context, int index) {
               return Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 150,
-                      child: Image.network(
-                        Item.items[3].imageUrl,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Text(Item.items[3].name)
-                  ],
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 120.0),
+                  ),
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://d2rbyiw1vv51io.cloudfront.net/web/ikea4/images/592/0959226_PE809439_S3.jpg"),
+                        fit: BoxFit.cover),
+                    border: Border.all(
+                      color: Color.fromRGBO(20, 99, 86, 1),
+                      width: 1,
+                    ), //Border.all
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  width: 150,
+                  height: 150,
                 ),
               );
             }));
